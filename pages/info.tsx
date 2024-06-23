92d15d5c074bd78e08567498157b7fbbaa6e4497
sameerdj445 <sameerdj445@gmail.com>
2024-06-23 03:37:19 +0000

    created states for info

--- pages/info.tsx
+++ pages/info.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Info = () => {
+  const [pages, setPages] = useState<number>(0)
+  const [complexity, setComplexity] = useState<number>(0)
+
   return (
     <div className='flex h-[100vh] flex-col'>
-      
+
     </div>
   )
 }
