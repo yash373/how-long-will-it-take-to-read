e5c835c09be32bf537e2c1344f1823348558cb74
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-23 07:29:26 +0530

    created info page

---
+++ pages/Info.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div>
+      takes info of the book (1. no.of pages, 2.how many times will you read a line over again or complexity)
+    </div>
+  )
+}
+
+export default Info
