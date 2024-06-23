f82efcab13795c7eede0f5141561ad6d7c0f5cdd
sameerdj445 <sameerdj445@gmail.com>
2024-06-23 04:14:56 +0000

    finished results page

--- pages/results.tsx
+++ pages/results.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import { useRouter } from 'next/router'
 
 const Results = () => {
+  const router = useRouter()
+
+  const pages: number = Number(router.query.pages)
+  const complexity: number = Number(router.query.complexity)
+
+  const secs_per_page = 102
+
+  const hours_for_read_once = (pages * secs_per_page) / (60 * 60)
+  const hours_for_read = hours_for_read_once * complexity
+
   return (
-    <div>
-      Showcase results for your inputs
+    <div className='font-bold text-center w-[100vw]'>
+      <p>
+        It will take {hours_for_read} hrs for you to read {pages} pages on average.
+      </p>
     </div>
   )
 }
