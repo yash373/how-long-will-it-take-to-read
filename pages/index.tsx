d54effdc6329f85297ffe7908877044f069b1894
Yashodhan Mandal <yash.yasodhan.123@gmail.com>
2024-06-23 07:48:55 +0530

    integrated title component into home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Title from '@/components/Homepage/Title'
 
 const Index = () => {
   return (
     <div>
-      home page
+      <Title title='how long will it take to read?' />
     </div>
   )
 }
