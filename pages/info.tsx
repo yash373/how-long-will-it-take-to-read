cf2610fee52a6e417ecce87b3ca7bf05cd518d5b
sameerdj445 <sameerdj445@gmail.com>
2024-06-23 03:31:16 +0000

    uninstalled jotai

--- pages/info.tsx
+++ pages/info.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Info = () => {
   return (
     <div className='flex h-[100vh] flex-col'>
-      takes info of the book (1. no.of pages, 2.how many times will you read a line over again or complexity)
+      
     </div>
   )
 }
